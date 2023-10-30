7ff99a15bcf5c1f44b1bc9559e1ca8ad97570ae6
Your Name <you@example.com>
2023-10-30 03:45:15 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Message from './Message'
+
+function App() {
+
+  return (
+    <div className="App">
+      {/* <h1>My First React App</h1> */}
+      <Message></Message>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Message.tsx
@@ -0,0 +1,7 @@
+
+function Message() {
+    // JavaScript XML
+    return <h1>Hello Ethiopia</h1>;
+}
+
+export default Message;
