a07d32900f9db165fe1c664344655b2e669f65a1
Your Name <you@example.com>
2023-10-30 07:31:02 -0700

    Just commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import ListGroup from './components/ListGroup'
-
+import Alert from './components/Alert'
 function App() {
+  const items = ["faith", "Hope", "Love"];
 
   return (
     <div className="App">
       <h1>My First React App</h1>
-      <ListGroup></ListGroup>
+      <ListGroup items={items} heading='But Love is greater than all' ></ListGroup>
+      <Alert>this is alert</Alert>
     </div>
   )
 }
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,19 +1,30 @@
 import { useState } from "react";
 
-function ListGroup() {
-    const items = ["faith", "Hope", "Love"];
-    let [index, setIndex] = useState(-1);
+interface Props{
+    items: string[];
+    heading: string;
+    // onSelect: (index: string) => string;
+}
+function ListGroup({items, heading}:Props) {
+  let [index, setIndex] = useState(-1);
 
   return (
     <>
-      <h1>List </h1>
+      <h1>{heading} </h1>
       <ul className="list-group">
         {items.map((item, i) => (
-            <li className={index === i ? "list-group-item active" : "list-group-item "} onClick={() => { setIndex(i) }}>
+          <li
+            className={
+              index === i ? "list-group-item active" : "list-group-item "
+            }
+            onClick={() => {
+              setIndex(i);
+            }}
+          >
             {item}
           </li>
         ))}
-      </ul>
+          </ul>
     </>
   );
 }
