63f322daf530ee41bd49acf4ee6de473d8d45084
Your Name <you@example.com>
2023-11-02 02:58:04 -0700

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,40 @@
-import './App.css'
-import ListGroup from './components/ListGroup'
-import Alert from './components/Alert'
+import "./App.css";
+import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+import { useState } from "react";
 function App() {
-  const items = ["faith", "Hope", "Love"];
+  // const [game, setGame] = useState({
+  //   id: 1,
+  //   player: {
+  //     name: "John"
+  //   }
+  // });
+
+  // const handleClick = () => {
+  //   setGame({ ...game, player: { ...game.player, name: "Bob" } });
+  // }
+
+  const [pizza, setPizza] = useState({
+    name: "Spicy Peperroni",
+    toppings: ["Mushroom"],
+  });
+  const handleClick = () => {
+    setPizza({ ...pizza, toppings: [...pizza.toppings, "Cheese"] });
+  }
 
   return (
     <div className="App">
       <h1>My First React App</h1>
-      <ListGroup items={items} heading='But Love is greater than all' onSelect={() => {  console.log("You Clicked!")}}></ListGroup>
+      <ListGroup
+        items={items}
+        heading="But Love is greater than all"
+        onSelect={() => {
+          console.log("You Clicked!");
+        }}
+      ></ListGroup>
       <Alert>this is alert</Alert>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
